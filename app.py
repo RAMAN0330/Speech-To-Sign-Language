e6a970ea698cb9728e486527f25a315fb6e3f7b4
Raman <83901587+RAMAN0330@users.noreply.github.com>
2024-06-10 18:45:09 +0530

    updated listening format and adding stoping to listening

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 from speech_to_text import SpeechToText
 from phrase_handler import PhraseHandler
@@ -6,24 +6,24 @@ from phrase_handler import PhraseHandler
 app = Flask(__name__)
 CORS(app)  # Enable CORS
 
-videos_directory = "path/to/Videos"
-alphabet_directory = "path/to/Alphabet"
+videos_directory = "Data-Set/Videos"
+alphabet_directory = "Data-Set/Alphabet"
 stt = SpeechToText()
 handler = PhraseHandler(videos_directory, alphabet_directory)
 
-@app.route('/listen', methods=['GET'])
-def listen():
-    audio_data = stt.listen()
-    return jsonify({"message": "Audio received, processing...", "status": "success"}), 200
-
 @app.route('/recognize', methods=['GET'])
 def recognize():
-    audio_data = stt.listen()
-    phrase = stt.recognize_speech(audio_data)
+    stt.listen()
+    return jsonify({"message": "Listening started, say something...", "status": "listening"}), 200
+
+@app.route('/stop', methods=['GET'])
+def stop():
+    stt.stop()
+    phrase = stt.recognize_speech()
     if phrase:
-        handler.search_and_play(phrase)
+        video_path, gif_path = handler.search_and_return_paths(phrase)
         handler.clear_cache()
-        return jsonify({"phrase": phrase, "status": "success"}), 200
+        return jsonify({"phrase": phrase, "video": video_path, "gif": gif_path, "status": "success"}), 200
     else:
         return jsonify({"message": "Sorry, I couldn't understand. Please try again.", "status": "error"}), 500
 
